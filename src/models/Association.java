6b167b3c5a0a860941a5b5a26926a2163b638d4a
TanyaMio <32582073+TanyaMio@users.noreply.github.com>
2019-12-16 22:58:44 +0200

    upload Association.java

---
+++ src/models/Association.java
@@ -0,0 +1,75 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "association")
+public class Association {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id")
+    private Task task;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artifact_id")
+    private Artifact artifact;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "artfrole_name")
+    private ArtfRole artfRole;
+
+    public Association(){}
+
+    public int getId(){
+        return id;
+    }
+
+    public Task getTask(){
+        return task;
+    }
+
+    public void setTask(Task t) {
+        this.task = t;
+    }
+
+    public Artifact getArtifact(){
+        return artifact;
+    }
+
+    public void setArtifact(Artifact a) {
+        this.artifact = a;
+    }
+
+    public ArtfRole getRole(){
+        return artfRole;
+    }
+
+    public void setArtfRole(ArtfRole ar){
+        this.artfRole = ar;
+    }
+
+    @Override
+    public String toString(){
+        return "Association:\nArtifact " + artifact +
+                "\nas " + artfRole +
+                "\nfor the " + task;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Association a = (Association) o;
+        return task.equals(a.task) &&
+                artifact.equals(a.artifact) &&
+                artfRole.equals(a.artfRole);
+    }
+}
