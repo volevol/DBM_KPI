83f072b5fc81c74736c7181cc3ca9276d528760f
TanyaMio <32582073+TanyaMio@users.noreply.github.com>
2019-12-16 22:57:07 +0200

    upload Task.java

---
+++ src/models/Task.java
@@ -0,0 +1,120 @@
+package models;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "deadline")
+    private String deadline;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<Assignment> assignments;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private List<Association> associations;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    public Task(){
+    }
+
+    public Task(String n, String d, String dl) {
+        this.name = n;
+        this.description = d;
+        this.deadline = dl;
+        assignments = new ArrayList<Assignment>();
+        associations = new ArrayList<Association>();
+    }
+
+    public void addAssignment(Assignment ag) {
+        ag.setTask(this);
+        assignments.add(ag);
+    }
+
+    public void removeAssingment(Assignment ag) {
+        assignments.remove(ag);
+    }
+
+    public void addAssociation(Association ac) {
+        ac.setTask(this);
+        associations.add(ac);
+    }
+
+    public void removeAssociation(Association ac) {
+        associations.remove(ac);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String d) {
+        this.description = d;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String dl) {
+        this.deadline = dl;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project p) {
+        this.project = p;
+    }
+
+    @Override
+    public String toString() {
+        return "Task \"" + name + "\":\n" +
+                "id = " + id +
+                "\ndescription = " + description +
+                "\nDeadline: " + deadline;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task t = (Task) o;
+        return name.equals(t.name) &&
+                description.equals(t.description) &&
+                deadline.equals(t.deadline);
+    }
+}
